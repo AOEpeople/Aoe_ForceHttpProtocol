e60b1fad18ca6e883943a5cceaa4b186a7a00984
Bastian Ike <bastian.ike@aoemedia.de>
2013-12-11 12:22:45 +0100

    fixes

--- app/code/community/Aoe/ForceHttpProtocol/Model/Observer.php
+++ app/code/community/Aoe/ForceHttpProtocol/Model/Observer.php
@@ -4,18 +4,18 @@ class Aoe_ForceHttpProtocol_Model_Observer
 {
     public function controllerActionPredispatch(Varien_Event_Observer $event)
     {
-        $http = array_filter(explode("\n", trim(Mage::getStoreConfig('system/aoe_forcehttpprotocol/http'))));
-        $https = array_filter(explode("\n", trim(Mage::getStoreConfig('system/aoe_forcehttpprotocol/https'))));
+        $http = array_filter(array_map('trim', explode("\n", trim(Mage::getStoreConfig('system/aoe_forcehttpprotocol/http')))));
+        $https = array_filter(array_map('trim', explode("\n", trim(Mage::getStoreConfig('system/aoe_forcehttpprotocol/https')))));
         $response = Mage::app()->getResponse();
         $request = Mage::app()->getRequest();
 
         if ($request->isSecure() && in_array($event->getControllerAction()->getFullActionName(), $http)) {
-            $response->setRedirect(str_replace('https://', 'http://', $request->getRequestUri()), 302);
-            $response->sendResponse();
+            header('Location: ' . str_replace('https://', 'http://', $request->getRequestUri()));
             exit;
         } else if (!$request->isSecure() && in_array($event->getControllerAction()->getFullActionName(), $https)) {
-            $response->setRedirect(str_replace('http://', 'https://', $request->getRequestUri()), 302);
-            $response->sendResponse();
+            //$response->setRedirect(str_replace('http://', 'https://', $request->getRequestUri()), 302);
+            //$response->sendResponse();
+            header('Location: ' . str_replace('http://', 'https://', $request->getRequestUri()));
             exit;
         }
     }
